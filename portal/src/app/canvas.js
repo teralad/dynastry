92eb2c35e1c9c99446fde7ceb4cb83c5cceeb517
Lee <leelakrishna463@gmail.com>
2024-07-28 03:53:49 +0530

    feat(0.0.2): Added Graph with increased node size

--- portal/src/app/canvas.js
+++ portal/src/app/canvas.js
@@ -1,123 +1,104 @@
+"use client";
+import * as d3 from "d3";
+import data from "@/../public/data/data.json";
 
-function getChart(){
-	// Specify the dimensions of the chart.
-	const width = 928;
-	const height = 680;
-  
-	// Specify the color scale.
-	const color = d3.scaleOrdinal(d3.schemeCategory10);
-  
-	// The force simulation mutates links and nodes, so create a copy
-	// so that re-evaluating this cell produces the same result.
-	const links = data.links.map(d => ({...d}));
-	const nodes = data.nodes.map(d => ({...d}));
-  
-	// Create a simulation with several forces.
-	const simulation = d3.forceSimulation(nodes)
-		.force("link", d3.forceLink(links).id(d => d.id))
-		.force("charge", d3.forceManyBody())
-		.force("x", d3.forceX())
-		.force("y", d3.forceY());
-  
-	// Create the SVG container.
-	const svg = d3.create("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("viewBox", [-width / 2, -height / 2, width, height])
-		.attr("style", "max-width: 100%; height: auto;");
-  
-	// Add a line for each link, and a circle for each node.
-	const link = svg.append("g")
-		.attr("stroke", "#999")
-		.attr("stroke-opacity", 0.6)
-	  .selectAll("line")
-	  .data(links)
-	  .join("line")
-		.attr("stroke-width", d => Math.sqrt(d.value));
-  
-	const node = svg.append("g")
-		.attr("stroke", "#fff")
-		.attr("stroke-width", 1.5)
-	  .selectAll("circle")
-	  .data(nodes)
-	  .join("circle")
-		.attr("r", 5)
-		.attr("fill", d => color(d.group));
-  
-	node.append("title")
-		.text(d => d.id);
-  
-	// Add a drag behavior.
-	node.call(d3.drag()
-		  .on("start", dragstarted)
-		  .on("drag", dragged)
-		  .on("end", dragended));
-	
-	// Set the position attributes of links and nodes each time the simulation ticks.
-	simulation.on("tick", () => {
-	  link
-		  .attr("x1", d => d.source.x)
-		  .attr("y1", d => d.source.y)
-		  .attr("x2", d => d.target.x)
-		  .attr("y2", d => d.target.y);
-  
-	  node
-		  .attr("cx", d => d.x)
-		  .attr("cy", d => d.y);
-	});
-  
-	// Reheat the simulation when drag starts, and fix the subject position.
-	function dragstarted(event) {
-	  if (!event.active) simulation.alphaTarget(0.3).restart();
-	  event.subject.fx = event.subject.x;
-	  event.subject.fy = event.subject.y;
-	}
-  
-	// Update the subject (dragged node) position during drag.
-	function dragged(event) {
-	  event.subject.fx = event.x;
-	  event.subject.fy = event.y;
-	}
-  
-	// Restore the target alpha so the simulation cools after dragging ends.
-	// Unfix the subject position now that it’s no longer being dragged.
-	function dragended(event) {
-	  if (!event.active) simulation.alphaTarget(0);
-	  event.subject.fx = null;
-	  event.subject.fy = null;
-	}
-  
-	// When this cell is re-run, stop the previous simulation. (This doesn’t
-	// really matter since the target alpha is zero and the simulation will
-	// stop naturally, but it’s a good practice.)
-	invalidation.then(() => simulation.stop());
-  
-	return svg.node();
-}
+import { useRef, useEffect } from "react";
 
-export default function ForceGraph() {
-	return (
-		<ForceGraph2D
-			graphData={{
-				nodes: [
-					{
-						id: "id1",
-						name: "name1",
-						val: 1
-					},
-					{
-						id: "id2",
-						name: "name2",
-						val: 10
-					}
-				],
-				links: [
-					{
-						source: "id1",
-						target: "id2"
-					}
-				]
-			}}
-		/>
-	);
+export default function Graph() {
+	const svgRef = useRef(null);
+
+	useEffect(() => {
+		// Specify the dimensions of the chart.
+		const width = "100svw";
+		const height = "100svh";
+
+		// Specify the color scale.
+		const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+		// The force simulation mutates links and nodes, so create a copy
+		// so that re-evaluating this cell produces the same result.
+		const links = data.links.map((d) => ({ ...d }));
+		const nodes = data.nodes.map((d) => ({ ...d }));
+
+		// Create a simulation with several forces.
+		const simulation = d3
+			.forceSimulation(nodes)
+			.force("link", d3.forceLink(links).id((d) => d.id).distance(200))
+			.force("charge", d3.forceManyBody().strength(-100))
+			.force("x", d3.forceX())
+			.force("y", d3.forceY())
+
+		// Create the SVG container.
+		const svg = d3
+			.select(svgRef.current)
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			.attr("viewBox", [-window.screen.width/2, -window.screen.height/2, window.screen.width, window.screen.height])
+			.attr("style", "max-width: 100%; max-height: 100%; border: 1px solid white;");
+
+		// Add a line for each link, and a circle for each node.
+		const link = svg
+			.append("g")
+			.attr("stroke", "#999")
+			.attr("stroke-opacity", 1)
+			.selectAll("line")
+			.data(links)
+			.join("line")
+			.attr("stroke-width", (d) => Math.pow(d.value, 2));
+
+		const node = svg
+			.append("g")
+			.attr("stroke", "#fff")
+			.attr("stroke-width", 1.5)
+			.selectAll("circle")
+			.data(nodes)
+			.join("circle")
+			.attr("r", 20)
+			.attr("fill", (d) => color(d.group));
+
+		node.append("title").text((d) => d.id);
+
+		// Add a drag behavior.
+		node.call(d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended));
+
+		// Set the position attributes of links and nodes each time the simulation ticks.
+		simulation.on("tick", () => {
+			link.attr("x1", (d) => d.source.x)
+				.attr("y1", (d) => d.source.y)
+				.attr("x2", (d) => d.target.x)
+				.attr("y2", (d) => d.target.y);
+
+			node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+		});
+
+		// Reheat the simulation when drag starts, and fix the subject position.
+		function dragstarted(event) {
+			if (!event.active) simulation.alphaTarget(1).restart();
+			event.subject.fx = event.subject.x;
+			event.subject.fy = event.subject.y;
+		}
+
+		// Update the subject (dragged node) position during drag.
+		function dragged(event) {
+			event.subject.fx = event.x;
+			event.subject.fy = event.y;
+		}
+
+		// Restore the target alpha so the simulation cools after dragging ends.
+		// Unfix the subject position now that it’s no longer being dragged.
+		function dragended(event) {
+			if (!event.active) simulation.alphaTarget(0);
+			event.subject.fx = null;
+			event.subject.fy = null;
+		}
+
+		// When this cell is re-run, stop the previous simulation. (This doesn’t
+		// really matter since the target alpha is zero and the simulation will
+		// stop naturally, but it’s a good practice.)
+		// invalidation.then(() => simulation.stop());
+		// return svg.node();
+	}, []);
+
+	return <div ref={svgRef}></div>;
 }
